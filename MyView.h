96aa45f51b03e17d2d2a9be987a88702c6be7cd4
wuxie8 <18763895506@163.com>
2018-08-23 15:49:03 +0800

    直接导入，改一下frame

---
+++ MyView.h
@@ -0,0 +1,20 @@
+//
+//  MyView.h
+//  DrawWall
+//
+//  Created by gll on 13-1-2.
+//  Copyright (c) 2013年 gll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@interface MyView : UIView
+@property(copy,nonatomic)NSString *tagStr;
+// get point  in view
+-(void)addPA:(CGPoint)nPoint;
+-(void)addLA;
+-(void)revocation;
+-(void)refrom;
+-(void)clear;
+-(void)setLineColor:(NSInteger)color;
+-(void)setlineWidth:(NSInteger)width;
+@end
